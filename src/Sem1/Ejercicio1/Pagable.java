426c31bcb54e3abee9ff5f68d502cbed95264f8c
LUO_BINGHE <regina.rodriguezcmpg@uanl.edu.mx>
2024-08-15 19:06:52 -0600

    Se comento algo

---
+++ src/Sem1/Ejercicio1/Pagable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Sem1.Ejercicio1;
+
+/**
+ *
+ * @author Regina
+ */
+public interface Pagable { //interfaz
+    
+    void RealizarPago();
+    
+}
