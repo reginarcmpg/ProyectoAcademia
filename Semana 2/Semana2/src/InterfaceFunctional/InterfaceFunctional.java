5906b2f230a76c15b505d40da4b6dca3d60dd28f
Regina <regina.rodriguezcmpg@uanl.edu.mx>
2024-08-24 14:39:15 -0600

    actaulizacion
    se termino el function

--- Semana 2/Semana2/src/InterfaceFunctional/InterfaceFunctional.java
+++ Semana 2/Semana2/src/InterfaceFunctional/InterfaceFunctional.java
@@ -8,7 +8,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class InterfaceFunctional {
@@ -17,7 +20,7 @@ public class InterfaceFunctional {
        
         
         System.out.println();
-        System.out.println("Supplier");
+        System.out.println("1.-Supplier");
         
         Supplier<LocalDateTime> s = () -> LocalDateTime.now();
         LocalDateTime time = s.get();
@@ -34,7 +37,7 @@ public class InterfaceFunctional {
 
         System.out.println();
         System.out.println("****************************************");
-        System.out.println("Consumer");
+        System.out.println("2.-Consumer");
 
         Consumer<String> message = msj ->  System.out.println(msj);
         message.accept("Hello");
@@ -51,7 +54,7 @@ public class InterfaceFunctional {
         
         System.out.println();
         System.out.println("****************************************");
-        System.out.println("BiConsumer");
+        System.out.println("3.-BiConsumer");
 
         BiConsumer<Integer, Integer> SumBiC = (x, y) -> System.out.println(x + y);
         SumBiC.accept(2, 5);
@@ -65,19 +68,65 @@ public class InterfaceFunctional {
         BiConsumer<Integer, Integer> BiCU = (x, y) -> System.out.println(compareUnsigned(x, y));
         BiCU.accept(8, 19); 
         
-      
+        System.out.println();
+        System.out.println("****************************************");
+        System.out.println("4.-Predicate");
         
+        Predicate<String> Letter = (Letter1) -> Letter1.contains("r");
+        boolean FinalLetter = Letter.test("Burger");
+        System.out.println(FinalLetter);
         
+        Predicate<String> Word = (Word1) -> Word1.startsWith("Gua");
+        boolean FinalWord = Word.test("Guacamole");
+        System.out.println(FinalWord);
+        
+         Predicate<String> PreLen = (x) -> x.length() > 12;
+         boolean FinalPreLen = PreLen.test("Guacamole");
+         System.out.println(FinalPreLen);
+        
+         Predicate<String> EmpPre = (x) -> x. isEmpty();
+         boolean FinalEmpPre = EmpPre.test("Guacamole");
+         System.out.println(FinalEmpPre);
+         
+        System.out.println();
+        System.out.println("****************************************");
+        System.out.println("5.-BiPredicate");
+        
+        BiPredicate<Integer, Integer> S1 = (s1, s2) ->  ((s1 + 2) > 5) && ((s2 + 5) > 20);
+        boolean FinalS =S1.test(3,7);
+        System.out.println(FinalS);
 
+        BiPredicate<String, Integer> ComNam = (Name1, numName) -> (numName >= Name1.length());
+        boolean LongName =ComNam.test("Miquelle",7);
+        System.out.println(LongName);
         
+        BiPredicate<String, String> Empty = (NameEm1, NameEm2) -> NameEm1.isEmpty() && NameEm2.isEmpty();
+        boolean Emptyname =Empty.test("Miquelle","");
+        System.out.println(Emptyname);
         
+        BiPredicate<String, String> Equal = (NameEq1, NameEq2) -> NameEq1.equals(NameEq2);
+        boolean Equalname =Equal.test("Miquelle","Miquelle");
+        System.out.println(Equalname);
         
+        System.out.println();
+        System.out.println("****************************************");
+        System.out.println("6.-Functions");
         
+        Function<String, Integer> LenFunN = x -> x.length();
+        Integer apply = LenFunN.apply("Miquelle");   
+        System.out.println(apply);
         
+         Function<Integer, Double> Dollar = x -> x * 18.10;
+         Double finalDoll = Dollar.apply(500); 
+         System.out.println(finalDoll);
         
+        Function<String, String> ConNameSt = x -> x + "Ring";
+        String FinRes = ConNameSt.apply("Elden"); 
+        System.out.println(FinRes);
         
-        
-        
+        Function<String, String> Low = x -> x.toUpperCase();
+        String Low1 = Low.apply("elden ring"); 
+        System.out.println(Low1);
     }
     
 }
