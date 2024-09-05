c1e8d29fa2f7f3af8254f968e46c5549cf90b99a
Regina <regina.rodriguezcmpg@uanl.edu.mx>
2024-09-05 16:15:15 -0600

    Controller Test

---
+++ Semana 4/SpringJPA/demo/src/main/java/com/spring/jpa/rest/BookController.java
@@ -0,0 +1,40 @@
+package com.spring.jpa.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/bookrest")
+public class BookController {
+
+	@GetMapping("/prueba")
+	public String getHello() {
+		return "Hola";
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
